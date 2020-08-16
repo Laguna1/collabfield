e126df655a9db918f77855abd9e6709702c47ac5
Laguna1 <lagunazoomarket@gmail.com>
2020-08-17 02:58:47 +0300

    Make search bar and category filters in branch pages functional - Add by_category, by_branch and search scopes in the Post model - Modify the get_posts method in PostsController

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,30 +1,44 @@
 class PostsController < ApplicationController
   before_action :redirect_if_not_signed_in, only: [:new]
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def hobby
+    posts_for_branch(params[:action])
+  end
+
+  def study
+    posts_for_branch(params[:action])
+  end
+
+  def team
+    posts_for_branch(params[:action])
+  end
+
+  private
+
+  def posts_for_branch(branch)
+    @categories = Category.where(branch: branch)
+    @posts = get_posts.paginate(page: params[:page])
+  end
+
+  def get_posts
+    branch = params[:action]
+    search = params[:search]
+    category = params[:category]
   
-    def show
-      @post = Post.find(params[:id])
+    if category.blank? && search.blank?
+      @posts = Post.by_branch(branch).all
+    elsif category.blank? && search.present?
+      @posts = Post.by_branch(branch).search(search)
+    elsif category.present? && search.blank?
+      @posts = Post.by_category(branch, category)
+    elsif category.present? && search.present?
+      @posts = Post.by_category(branch, category).search(search)
+    else
     end
+  end
 
-    def hobby
-      posts_for_branch(params[:action])
-    end
-    
-    def study
-      posts_for_branch(params[:action])
-    end
-    
-    def team
-      posts_for_branch(params[:action])
-    end
-
-    private
-
-    def posts_for_branch(branch)
-      @categories = Category.where(branch: branch)
-      @posts = get_posts.paginate(page: params[:page])
-    end
-
-    def get_posts
-      Post.limit(30)
-    end
 end
