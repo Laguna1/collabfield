6a9f17d109c95b3828068868e2fcf215472aa8b8
Laguna1 <lagunazoomarket@gmail.com>
2020-08-17 02:43:15 +0300

    Add a search form in branch pages - Render a search form inside the _branch.html.erb - Create a _search_form.html.erb partial file - Define a category_field_partial_path helper method in PostsHelper - Create a _category_field.html.erb partial file - Add CSS for the the search form in branch_page.scss

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,3 +1,4 @@
+ 
 // This is a manifest file that'll be compiled into application.js, which will include all the files
 // listed below.
 //
@@ -10,9 +11,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-ujs
-//= require activestorage
+//= require jquery
+//= require jquery_ujs
+//= require bootstrap/dropdown
+//= require bootstrap-sprockets
 //= require turbolinks
 //= require_tree .
-//= require jquery
-//= require bootstrap-sprockets
--- app/assets/javascripts/posts/modal.js
+++ app/assets/javascripts/posts/modal.js
@@ -12,3 +12,4 @@ $(document).on('turbolinks:load', function() {
       $('.myModal').modal('show');
     });
   });
+  
