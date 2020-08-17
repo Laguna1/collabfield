5d0993eb9568377af68cddfd64808418539cd841
Laguna1 <lagunazoomarket@gmail.com>
2020-08-17 03:51:12 +0300

    Add feature specs for posts' infinite scroll functionality

--- db/schema.rb
+++ db/schema.rb
@@ -30,7 +30,16 @@ ActiveRecord::Schema.define(version: 2020_08_16_194608) do
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
-# Could not dump table "users" because of following StandardError
-#   Unknown type 'srting' for column 'name'
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
 
 end
